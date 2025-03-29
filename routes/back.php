88e1b5fd32d760aa725dd3a1b1a1358bba4c79e3
f3brysan <febrysan1995@gmail.com>
2025-03-30 02:12:04 +0700

    Add SCSS and JS files for Bootstrap, Highlight.js, and custom components

--- routes/back.php
+++ routes/back.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Backend\B_DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('dashboard', [B_DashboardController::class, 'index']); 
