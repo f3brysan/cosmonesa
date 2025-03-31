9c7690c4d14861ee67f95f141acf720f5b2fe85f
synysmike <sembuarang@yahoo.com>
2025-04-01 03:42:46 +0700

    fox unmerged route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Frontend\F_DashboardController;
 // });
 
 Route::get('/', [F_DashboardController::class, 'index']);
+Route::get('/book', [F_AppointmentController::class, 'index']);
 =======
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +20,6 @@ Route::get('/login', function () {
 });
 
 // oauth
-Route::get('oauth/google', [\App\Http\Controllers\OauthController::class, 'redirectToProvider'])->name('oauth.google');  
+Route::get('oauth/google', [\App\Http\Controllers\OauthController::class, 'redirectToProvider'])->name('oauth.google');
 Route::get('oauth/google/callback', [\App\Http\Controllers\OauthController::class, 'handleProviderCallback'])->name('oauth.google.callback');
 >>>>>>> 0bf6e880d17c2fd3f052f80c814c19942488f275
