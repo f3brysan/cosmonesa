8b5a785c7134cf3310680d4981da6a0b08d5e872
f3brysan <febrysan1995@gmail.com>
2025-04-02 06:12:12 +0700

    Fix route definition for homepage by removing commented code and resolving merge conflict

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\F_DashboardController;
 
-<<<<<<< HEAD
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
 Route::get('/', [F_DashboardController::class, 'index']);
 Route::get('/book', [F_AppointmentController::class, 'index']);
-=======
 Route::get('/', function () {
     return view('welcome');
 });
@@ -22,4 +20,3 @@ Route::get('/login', function () {
 // oauth
 Route::get('oauth/google', [\App\Http\Controllers\OauthController::class, 'redirectToProvider'])->name('oauth.google');
 Route::get('oauth/google/callback', [\App\Http\Controllers\OauthController::class, 'handleProviderCallback'])->name('oauth.google.callback');
->>>>>>> 0bf6e880d17c2fd3f052f80c814c19942488f275
