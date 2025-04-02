ade22bf0e14fc2d5dd24a146da4f7b0411fbe732
f3brysan <febrysan1995@gmail.com>
2025-04-02 07:44:02 +0700

    Enhance OauthController to handle user creation and role assignment during Google OAuth callback

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -10,42 +10,59 @@ use Laravel\Socialite\Facades\Socialite;
 class OauthController extends Controller
 {
     public function redirectToProvider()
-    {        
+    {
         return Socialite::driver('google')->redirect();
     }
-    public function handleProviderCallback()
+    /**
+     * Handle the callback from Google OAuth.
+     * 
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function handleProviderCallback(): \Illuminate\Http\RedirectResponse
     {
         try {
-            
-            $user = Socialite::driver('google')->user();             
+            // Get the user from the OAuth provider
+            $user = Socialite::driver('google')->user();
+
+            // Check if the user already exists in the database
             $finduser = User::where('gauth_id', $user->id)->first();
-            
-            if($finduser){
 
-                $getUser = Auth::login($finduser);
+            if ($finduser) {
+                // If the user exists, login the user
+                Auth::login($finduser);
 
-                if($finduser->hasRole(['superadmin', 'pengelola', 'seller'])){
+                // Redirect the user to the correct dashboard
+                if ($finduser->hasRole(['superadmin', 'pengelola', 'seller'])) {
                     return redirect()->intended('back/dashboard');
                 }
-                
-                return redirect('/dashboard');
 
-            }else{
+                return redirect('/dashboard');
+            } else {
+                // If the user does not exist, create a new user
+
+                // Generate a random password for the new user
+                $password = strstr($user->email, '@', true);
+
+                // Create a new user
                 $newUser = User::create([
                     'name' => $user->name,
                     'email' => $user->email,
-                    'gauth_id'=> $user->id,
-                    'gauth_type'=> 'google',
-                    'password' => encrypt('cosmonesa!@#')
+                    'gauth_id' => $user->id,
+                    'gauth_type' => 'google',                    
+                    'password' => encrypt($password),
                 ]);
 
+                // Assign the customer role to the new user
                 $newUser->assignRole('customer');
 
+                // Login the new user
                 Auth::login($newUser);
 
+                // Redirect the new user to the customer dashboard
                 return redirect('/dashboard');
             }
         } catch (\Exception $th) {
+            // If there is an error, display the error message
             dd($th->getMessage());
         }
     }
